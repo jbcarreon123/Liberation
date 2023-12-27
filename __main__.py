2da4f8c6b05bf412c50ea881861e63526e54e721
Gowthr <taylorlickwid@gmail.com>
2023-12-27 04:06:31 -0500

    Removed old comments

--- __main__.py
+++ __main__.py
@@ -8,7 +8,8 @@ from disnake.ext import commands
 command_sync_flags = commands.CommandSyncFlags.default()
 command_sync_flags.allow_command_deletion = True
 command_sync_flags.sync_commands = True
-bot = commands.Bot(command_prefix='*', command_sync_flags=command_sync_flags, intents=disnake.Intents.default() | disnake.Intents.message_content)
+bot = commands.Bot(command_prefix='*', command_sync_flags=command_sync_flags,
+                   intents=disnake.Intents.default() | disnake.Intents.message_content)
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s: %(message)s')
@@ -17,25 +18,28 @@ database.connect()
 
 database.create_table("config", ["guildId", "categoryId"])
 
+
 @bot.event
 async def on_ready():
     logging.info(f'Bot ready! Logged in as {bot.user.name}.')
 
-#TODO Write help command
-@bot.slash_command(name='help',  description='Command usage and explanation')
+
+# TODO: Write help command
+@bot.slash_command(name='help', description='Command usage and explanation')
 async def help_slash(inr: disnake.ApplicationCommandInteraction):
-    await inr.response.send_message(f'~~~')
+    await inr.response.send_message('~~~')
     logging.info('nothing')
 
 
 @bot.slash_command(name='ping', description='Ping')
 async def ping_slash(inr: disnake.ApplicationCommandInteraction):
-    await inr.response.send_message(f'Pong! Latency is {(bot.latency*1000).__round__(2)}s!')
+    await inr.response.send_message(f'Pong! Latency is {(bot.latency * 1000).__round__(2)}s!')
 
 
 @bot.command(name='ping')
 async def ping_prefix(inr: disnake.CommandInteraction):
-    await inr.send(f'Pong! Latency is {(bot.latency*1000).__round__(2)}s!', reference=inr.message, allowed_mentions=disnake.AllowedMentions.none())
+    await inr.send(f'Pong! Latency is {(bot.latency * 1000).__round__(2)}s!', reference=inr.message,
+                   allowed_mentions=disnake.AllowedMentions.none())
 
 
 @bot.slash_command(name='configure', description='Configure the bot')
@@ -44,11 +48,11 @@ async def conf_slash(inr: disnake.ApplicationCommandInteraction, category: disna
     await inr.response.send_message('<:done:1189216626604769320> Done!')
 
 
-#TODO Add category to create_voice_channel
-#Create control panel text channel
+# TODO: Create control panel text channel
 @bot.slash_command(name='create', description='Create a party VC')
 async def create_party(inr: disnake.ApplicationCommandInteraction, display_name: str):
-    await inr.response.send_message('<:pending:1189216631268843530> Creating a party VC...\n<:desc:1189216629519826975> Administrators or people that have the `ADMINISTRATOR` permission can see party VCs, regardless of its privacy level!')
+    await inr.response.send_message(
+        '<:pending:1189216631268843530> Creating a party VC...\n<:desc:1189216629519826975> Administrators or people that have the `ADMINISTRATOR` permission can see party VCs, regardless of its privacy level!')
     categories = database.select("config", columns=['guildId', 'categoryId'], condition=f'guildId = {inr.guild_id}')
     category = categories[0][1]
     await inr.guild.create_voice_channel(name=display_name + ' (Party)', category=inr.guild.get_channel(category))
@@ -58,12 +62,13 @@ async def create_party(inr: disnake.ApplicationCommandInteraction, display_name:
 
 @bot.command(name='gowthr')
 async def ee1(inr: disnake.CommandInteraction):
- await inr.send('gowthr was here...', reference=inr.message, allowed_mentions=disnake.AllowedMentions.none())
+    await inr.send('gowthr was here...', reference=inr.message, allowed_mentions=disnake.AllowedMentions.none())
 
 
 @bot.command(name='krunged')
 async def ee2(inr: disnake.CommandInteraction):
- await inr.send('GET A JUICY CHICKEN SANDWICH TODAY 50% OFF IF YOU USE CODE \"KRUNGED!\" \n\n*Deal has expired*', reference=inr.message, allowed_mentions=disnake.AllowedMentions.none())
+    await inr.send('GET A JUICY CHICKEN SANDWICH TODAY 50% OFF IF YOU USE CODE \"KRUNGED!\" \n\n*Deal has expired*',
+                   reference=inr.message, allowed_mentions=disnake.AllowedMentions.none())
 
 
 bot.run('[BOT TOKEN HERE]')
